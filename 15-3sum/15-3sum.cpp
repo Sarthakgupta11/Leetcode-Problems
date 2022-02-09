56e36e042f4a12ebe150f99c806f98334ee7efe5
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-02-09 21:55:12 +0530

    Time: 199 ms (28.15%), Space: 21.2 MB (38.77%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,43 +2,44 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& a) 
     {
-        sort(a.begin(),a.end());
-        int n=a.size();
-        
         vector<vector<int>>ans;
         
-        for(int i=0;i<n;i++)
-        {
-            int l=i+1;
-            int r=n-1;
-            
-            if(i>0 && a[i]==a[i-1])
-                continue;
-            
-            while(l<r)
-            {
-                vector<int>x;
-                if(a[i]+a[l]+a[r]==0)
-                {
-                    x.push_back(a[i]);
-                    x.push_back(a[l]);
-                    x.push_back(a[r]);
-                    ans.push_back(x);
-                    
-                    while(l<r-1 && a[l]==a[l+1])
-                        l++;
-                    while(l+1<r && a[r]==a[r-1])
-                        r--;
-                    l++;
-                    r--;
-                }
-                else if(a[i]+a[l]+a[r]<0)
-                l++;
-                else
-                r--;
-            }
-        }
+        sort(a.begin(),a.end());
         
-        return ans;
+        if(a.size()<3)
+          return ans;
+        
+        for(int x=0;x<a.size()-2;x++)
+        {
+            while(x<a.size() && x>0 && a[x]==a[x-1])
+            {
+                x++; 
+                continue;
+            }
+            
+          int i=x+1;
+          int j=a.size()-1;
+            while(i<j)
+            {
+                
+                if(a[i]+a[j]+a[x]==0 && i<j)
+                {
+                    vector<int>z{a[x],a[i],a[j]};
+                    ans.push_back(z);
+                     //cout<<1<<" ";
+                    while(j>i && a[j]==a[j-1])
+                        j--;
+                    i++;
+                    j--;
+                }
+                else if(a[i]+a[j]+a[x]>0 && i<j)
+                    j--;
+                else if(i<j)
+                    i++;
+            }
+        }    
+        
+        
+        return ans;      
     }
 };
