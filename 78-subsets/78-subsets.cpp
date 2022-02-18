10ab05fb3189d6336c6d1a0038ae4986dc82f6f7
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-02-18 21:59:01 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (64.72%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    
-    void solve(int i, vector<int>&nums,vector<int>&x,int n)
-    {
-        if(i==n)
-        {
-            ans.push_back(x);
+    void f(int ind,vector<int>&dp,vector<int>&nums,vector<vector<int>>&arr,int n){
+        if(ind>=n){
+            arr.push_back(dp);
             return;
         }
-        
-        x.push_back(nums[i]);
-        solve(i+1,nums,x,n);
-        x.pop_back();
-        solve(i+1,nums,x,n);
-        
-        return;
+        dp.push_back(nums[ind]);
+        f(ind+1,dp,nums,arr,n);
+        dp.pop_back();
+        f(ind+1,dp,nums,arr,n);
     }
-    vector<vector<int>> subsets(vector<int>& nums) 
-    {
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>arr;
+        vector<int>dp;
         int n=nums.size();
-        vector<int>x;
-        solve(0,nums,x,n);
-        return ans;   
+        f(0,dp,nums,arr,n);
+        return arr;
     }
 };
