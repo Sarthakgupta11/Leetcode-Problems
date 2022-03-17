6a9798c2dfda34f97c853796e66e745b31ed5edb
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-03-17 12:12:16 +0530

    Time: 76 ms (24.30%), Space: 46.8 MB (16.32%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     void solve(int i,vector<int>&a,int k,vector<vector<int>>&ans,vector<int>ds)
     {   
+        if(i==a.size())
+        {
+            
         if(k==0)
         {
             ans.push_back(ds);
+        }
             return;
         }
-        if(i==a.size())
-        {
-            return;
-        }
-        solve(i+1,a,k,ans,ds); 
         if(a[i]<=k)
         {
             ds.push_back(a[i]);
         solve(i,a,k-a[i],ans,ds);
+            ds.pop_back();
         } 
+                solve(i+1,a,k,ans,ds); 
+
         
     }
     
