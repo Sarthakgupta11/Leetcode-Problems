a2defd45f0025244238e6e3bfdf0f0169988e9b5
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-05-04 16:52:41 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.77%) - LeetHub

---
+++ 878-nth-magical-number/878-nth-magical-number.cpp
@@ -0,0 +1,28 @@
+
+#define ll long long
+
+class Solution {
+public:
+    int nthMagicalNumber(int n, int a, int b) 
+    {
+        ll l=2;
+        ll r= 1e18;
+        ll ans;
+        
+        while(l<r)
+        {
+            ll mid=(l+r)/2;
+            ll lcm=a*b/__gcd(a,b);
+            ans=mid/a + mid/b - mid/lcm;
+            
+            if(ans<n)
+                l=mid+1;
+            else
+            {
+                r=mid;
+            }
+            
+        }         
+            return l%1000000007;       
+    }
+};
