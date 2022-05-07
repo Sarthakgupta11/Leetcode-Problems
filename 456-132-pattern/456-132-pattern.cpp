1c51e70102ec82a93531d3e96893860d710b9abf
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-05-07 23:37:46 +0530

    Time: 81 ms (77.10%), Space: 38 MB (94.28%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) 
+    {
+        stack<int>s;
+        
+        int t=INT_MIN;
+        
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            if(nums[i]<t)
+                return true;
+            else
+            {
+                while(!s.empty() && s.top()<nums[i])
+                {
+                    t=s.top();
+                    s.pop();
+                }
+            }
+            s.push(nums[i]);
+        }
+        
+        return false;
+    }
+};
