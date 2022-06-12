11fecbda08d41acdf2cdcf4286cb4105a6d40125
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-06-12 23:41:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.11%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int dp[31];
     
-    int rec(int n)
+    int fib(int n)
     {
         if(n==0 || n==1)
             return n;
         
-        return dp[n]=rec(n-1)+rec(n-2);
-    }
-    
-    int fib(int n)
-    {
-        memset(dp,-1,sizeof(dp));
-        return rec(n);   
+        int a=0;
+        int b=1;
+        int ans=0;
+        
+        for(int i=0;i<=n-2;i++)
+        {
+            ans=a+b;
+            a=b;
+            b=ans;
+        }
+        return ans;   
     }
 };
