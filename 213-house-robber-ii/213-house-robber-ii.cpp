322ee60b9d2560568d1c10987961800182c7c717
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-06-21 16:24:57 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (84.33%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int dp[101];
     
-    int solve(int i,vector<int>&a)
+    int solve(int i,vector<int>&a,int size)
     {
-        if(i<0)
+        if(i>=size)
             return 0;
         
         if(dp[i]!=-1)
             return dp[i];
         
-        return dp[i]=max(solve(i-1,a),a[i]+solve(i-2,a));
+        return dp[i]=max(solve(i+1,a,size),a[i]+solve(i+2,a,size));
     }    
     
     int rob(vector<int>& a) 
@@ -22,16 +22,11 @@ public:
         
         if(n==2)
             return max(a[1],a[0]);
-        
-        vector<int>b;
-        
-        for(int i=1;i<n;i++)
-        b.push_back(a[i]);
-        
+                
         memset(dp,-1,sizeof(dp));
-        int x=solve(n-2,a);
+        int x=solve(0,a,n-1);
         memset(dp,-1,sizeof(dp));
-        int y=solve(n-2,b);
+        int y=solve(1,a,n);
         
     return max(x,y);
     }
