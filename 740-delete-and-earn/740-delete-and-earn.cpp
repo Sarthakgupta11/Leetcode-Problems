7d28ebfc0e1454a5d23b24e7bfad32ddcc556a1c
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-06-21 16:44:09 +0530

    Time: 30 ms (31.08%), Space: 17.7 MB (11.35%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[20001];
+    
+    int solve(int i,vector<int>&a,int size)
+    {
+        if(i>=size)
+            return 0;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        return dp[i]=max(solve(i+1,a,size),a[i]+solve(i+2,a,size));
+    }
+    
+    
+    int deleteAndEarn(vector<int>& nums) 
+    {
+        vector<int>a(20001,0);
+        
+        for(int i=0;i<nums.size();i++)
+            a[nums[i]]+=nums[i];
+        
+        memset(dp,-1,sizeof(dp));        
+        return solve(0,a,a.size());
+    }
+};
