fbc6b862a2d684180c2ebe2f0ac9d1c5b6d11add
Sarthak Gupta <61046702+Sarthakgupta11@users.noreply.github.com>
2022-09-02 16:25:04 +0530

    Time: 26 ms (39.71%), Space: 9.8 MB (84.39%) - LeetHub

---
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int dp[100][100];
+    
+    int rec(int r,int c,vector<vector<int>>&matrix)
+    {
+        if(c<0 || c>=matrix[0].size())
+            return INT_MAX;
+        
+        if(dp[r][c]!=-1)
+            return dp[r][c];
+        
+        if(r==0)
+            return matrix[r][c];
+        
+              
+        return dp[r][c]=matrix[r][c]+ min({rec(r-1,c-1,matrix),rec(r-1,c,matrix),rec(r-1,c+1,matrix)});
+    }
+    
+    
+    
+    int minFallingPathSum(vector<vector<int>>& matrix) 
+    {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        
+        memset(dp,-1,sizeof(dp));
+        
+        int ans=INT_MAX;
+        
+        for(int i=0;i<col;i++)
+        {
+            ans=min(ans,rec(row-1,i,matrix));
+        }       
+                
+        return ans;
+    }
+};
